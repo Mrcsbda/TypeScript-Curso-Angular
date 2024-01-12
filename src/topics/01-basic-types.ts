e705fb66afb94db8d826af88af38262e431887b8
Mrcsbda <macbedoya@gmail.com>
2024-01-12 14:18:17 -0500

    first commit

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+
+const name: string = "Mariana"
+let hpPoints: number | string = 95
+let hpPoints2: number | "Full" = 95
+let isAlive: boolean = true
+
+console.log({
+    name,
+    hpPoints,
+    hpPoints2,
+    isAlive
+})
+
+ export {};
---
+++ src/topics/05-basic-destructuring.ts
@@ -0,0 +1,31 @@
+interface AudioPlayer {
+    audioVolumen: number
+    songDuration: number,
+    song: string,
+    details: Details
+}
+
+interface Details {
+    author: string;
+    year: number
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolumen: 90,
+    songDuration: 36,
+    song: "Mess",
+    details: {
+        author: "Ed Sheeran",
+        year: 2015
+    }
+}
+//desestructuración de objetos
+const { song:anotherSong, audioVolumen } = audioPlayer;
+//forma 1 para desestructurar un objeto dentro de un objeto
+const { details } = audioPlayer;
+const { author } = details
+//forma 2 para desestructurar un objeto dentro de un objeto
+const { details: { year } } = audioPlayer;
+
+console.log(anotherSong, year, author, audioVolumen)
+export { };
